0698298c3c70665c451407c95159ab4e02ef1da9
Ostap4ik777 <137522973+Ostap4i@users.noreply.github.com>
2024-05-20 20:52:37 +0300

    Add files via upload

---
+++ Project_Hotel/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Project_Hotel
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
